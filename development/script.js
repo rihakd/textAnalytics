1942c754a3ca999702557f2c3013d7e23a439b06
David Řihák <davidrihak@inside-65-86-200.wireless.utoronto.ca>
2015-07-17 20:15:55 -0400

    Front-end beta ver 1 finished.

--- development/script.js
+++ development/script.js
@@ -2,6 +2,8 @@
 var panelInUse = false;
 var exapandedWordEntry = "";
 var exapandedWordEntryDiv = null;
+var $focusedWord;
+
 //jQuery
 $(document).ready(function(){
 	
@@ -20,6 +22,7 @@ $(document).ready(function(){
 		},
 		click: function(){
 			wordClick(this);
+			scrllAnim();
 		}
 	}, ".wordEntry");
 
@@ -48,10 +51,77 @@ $(document).ready(function(){
 		function(){
 			wordClick(this);
 	});
+	
+	$("body").on("click", ".next", function(){
+		// somthing has to be selected!
+		nextFocus();
+	});
 
+	$("body").on("click", ".prev", function(){
+		// somthing has to be selected!
+		prevFocus();
+	});
+	
 });
 
 
+// .js functions
+function prevFocus(){
+	if ($focusedWord != null){
+		$p = $focusedWord.closest("p");
+		if ($focusedWord.prevAll(".clickedInText").last().length > 0){
+			$focusedWord.removeClass("focusedInText");
+			$focusedWord.prevAll(".clickedInText").first().addClass("focusedInText");
+			$focusedWord = $focusedWord.prevAll(".clickedInText").first();
+		} else {
+			// $(obj).closest('tr').nextAll(':has(.class):first').find('.class');
+			if($p.prevAll(":has(.clickedInText):first").length > 0){
+				$prevT = $p.prevAll(":has(.clickedInText):first")
+								.find(".clickedInText");
+				$prevT.last().addClass("focusedInText");
+				$focusedWord.removeClass("focusedInText");
+				$focusedWord = $prevT;
+
+			} else {
+				$focusedWord.removeClass("focusedInText");
+				addCssToLast("focusedInText", ".clickedInText", $focusedWord);				
+			}
+		}
+		scrllAnim();
+	}
+}
+
+function nextFocus(){
+	if ($focusedWord != null){
+		$p = $focusedWord.closest("p");
+		if ($focusedWord.nextAll(".clickedInText").first().length > 0){
+			$focusedWord.removeClass("focusedInText");
+			$focusedWord.nextAll(".clickedInText").first().addClass("focusedInText");
+			$focusedWord = $focusedWord.nextAll(".clickedInText").first();
+		} else {
+			// $(obj).closest('tr').nextAll(':has(.class):first').find('.class');
+			if($p.nextAll(":has(.clickedInText):first").length > 0){
+				$nextT = $p.nextAll(":has(.clickedInText):first")
+								.find(".clickedInText");
+				$nextT.addClass("focusedInText");
+				$focusedWord.removeClass("focusedInText");
+				$focusedWord = $nextT;
+
+			} else {
+				$focusedWord.removeClass("focusedInText");
+				addCssToFirst("focusedInText", ".clickedInText", $focusedWord);				
+			}
+			scrllAnim();
+		}
+	}
+}
+
+//scroll
+function scrllAnim(){
+	$("#text").animate({
+		scrollTop: $(".focusedInText").offset().top - $("#text").offset().top + $("#text").scrollTop()
+	});
+}
 
 function wordClick(word){
 	var $t = $(word).text();
@@ -61,14 +131,11 @@ function wordClick(word){
 	//		so it should probably be the first case 
 	// 		since it shares some bahaviour
 	if(!panelInUse){
+		addCssToFirst("focusedInText", ".word", word);
 		addCssToClass("clickedInText", ".word", word);
 		// add an info pop-up box
 		if (!existsIn(word ,".wordEntry")){
-			$("#panel").prepend(
-				'<div class="wordEntry">' 
-				+ $t
-				+ '<button class="delButton btn '
-				+ 'btn-danger"></button></div>');
+			addWordEntry($t);
 		}
 		addCssToClass("buttonOn", ".wordEntry", word);
 		addAfter("<p id='info'>15% bitch</p>", ".wordEntry", word);
@@ -85,6 +152,7 @@ function wordClick(word){
 		// highlish words in text
 		removeCssFromClass("buttonOn", ".wordEntry", word);
 		removeCssFromClass("clickedInText", ".word", word);
+		removeCssFromClass("focusedInText", ".word", word);
 		// remove the info box
 		$("#info").remove();
 		// Finish by updateing page status
@@ -101,25 +169,39 @@ function wordClick(word){
 	//						add a new entry
 
 	} else {
+		removeCssFromClass("focusedInText", ".word", exapandedWordEntryDiv);
 		removeCssFromClass("buttonOn", ".wordEntry", exapandedWordEntryDiv);
 		removeCssFromClass("clickedInText", ".word", exapandedWordEntryDiv);
 		$("#info").remove();
 		exapandedWordEntry = $t;
 		exapandedWordEntryDiv = word;
 		if (!existsIn(word ,".wordEntry")){
-			$("#panel").prepend(
-				'<div class="wordEntry">' 
-				+ $t
-				+ '<button class="delButton btn '
-				+ 'btn-danger"></button></div>');
+			addWordEntry($t);
 		}
+		addCssToFirst("focusedInText", ".word", word);
 		addCssToClass("clickedInText", ".word", word);
 		addCssToClass("buttonOn", ".wordEntry", word);
 		addAfter("<p id='info'>15% bitch</p>", ".wordEntry", word);
 	}	
 }
 
-// .js functions 
+/*
+Adds a .wordEntry to the panel with textStr as text.
+*/
+function addWordEntry(textStr){
+	$("#panel").prepend(
+		'<div class="wordEntry">'
+		+ textStr
+		+ '<button class="delButton btn btn-danger">'
+		+ '<span class="glyphicon glyphicon-remove">'
+		+ '</span>'
+		+ '</button>'
+		+ '</div>');
+}
+
+/*
+Delete .wordEntry from the panel.
+*/
 function fDelButton(buttonObject){
 	// var $t = $(this).parent().text();
 	// the problem is: once i click remove
@@ -135,6 +217,28 @@ function fDelButton(buttonObject){
 	removeCssFromClass("selected", 
 		".word", $(buttonObject).parent());
 	$(buttonObject).parent().remove();
+}
+/*
+Adds focus css on the first occurance of the word in text.
+*/
+function addCssToFirst(css, clss, word){
+	var $t = $(word).text();
+	var $frst = $(clss).filter(function(){
+		return $(this).text() == $t;
+	}).first();
+	$frst.addClass(css);
+	$focusedWord = $frst;
+	// return the span, so it can be saved and worked with later.
+}
+
+function addCssToLast(css, clss, word){
+	var $t = $(word).text();
+	var $lst = $(clss).filter(function(){
+		return $(this).text() == $t;
+	}).last();
+	$lst.addClass(css);
+	$focusedWord = $lst;
+	// return the span, so it can be saved and worked with later.
 }
 
 /*
@@ -185,6 +289,10 @@ function addAfter(target, clss, word){
 	}).after(target);
 }
 
+/*
+returns the filtered list with clsses that have the 
+word text in them. returns 0 if non present.
+*/
 function existsIn(word, clss){
 	var $t = $(word).text();
 	return $(clss).filter(function(){
